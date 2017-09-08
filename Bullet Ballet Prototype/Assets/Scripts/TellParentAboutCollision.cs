37b6833a2d1738135636d48b6e245b97d8c4689c
James Owen <jamesowen012@gmail.com>
2017-09-08 15:21:38 +1000

    Enemys will now appear after they walk out of their room
    new script to invoke a event when it's trigger gets entered (needs to also have OnCollisionEnter and the exit versions of both)
    removed a old comment that doesnt belong there in PlayerPathing

---
+++ Bullet Ballet Prototype/Assets/Scripts/TellParentAboutCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityEventTrigger : UnityEvent<Collider> { }
+
+public class TellParentAboutCollision : MonoBehaviour {
+    public UnityEventTrigger m_TriggerEnter;
+
+    public void OnTriggerEnter(Collider other) {
+        m_TriggerEnter.Invoke(other);
+    }
+
+}
