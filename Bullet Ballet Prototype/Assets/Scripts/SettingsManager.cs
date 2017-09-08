dbe62028a3c34bcc60a7f67ac03e85c83b2c5bcf
Addison Stewart <addison.stewart@hotmail.com>
2017-09-08 13:48:47 +1000

    Finished options menu funcitonality and moved post processing to resources folder

--- Bullet Ballet Prototype/Assets/Scripts/SettingsManager.cs
+++ Bullet Ballet Prototype/Assets/Scripts/SettingsManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using UnityEngine.PostProcessing;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -10,9 +11,13 @@ public class SettingsManager : MonoBehaviour
     public GameObject soundsMenu;
 
     public Toggle fullscreenToggle;
+    public Toggle vSyncToggle;
+    public Toggle motionBlueToggle;
+    public Toggle postProcessingToggle;
     public Dropdown resolutionDropdown;
     public Dropdown textureQualityDropdown;
     public Dropdown antialiasingDropdwon;
+    public Slider gammaSlider;
     public Slider masterVolumeSlider;
     public Slider bgmMusicVolumeSlider;
     public Slider sfxVolumeSlider;
@@ -21,62 +26,115 @@ public class SettingsManager : MonoBehaviour
     public Toggle sfxMuteToggle;
     public Button applyButton;
 
+
+    public bool test;
+
+    //********************************************************* Post Processing variables *********************************************
+
+    public PostProcessingProfile postProcessing;
+
+        
+    //*******************************************************************************************************************************
+
     public Resolution[] resolutions;
 
     public GameSettings gameSettings;
 
     SoundManager soundMan;
 
-    void Start()
+    void Awake()
     {
-
-        graphicsMenu = GameObject.Find("Graphics");
-        soundsMenu = GameObject.Find("Sounds");
-
+       
         gameSettings = new GameSettings();
 
+       
+            fullscreenToggle = GameObject.Find("Fullscreen").GetComponent<Toggle>();
+            vSyncToggle = GameObject.Find("Verticle Sync").GetComponent<Toggle>();
+            motionBlueToggle = GameObject.Find("Motion Blur").GetComponent<Toggle>();
+            postProcessingToggle = GameObject.Find("Post Processing").GetComponent<Toggle>();
+            resolutionDropdown = GameObject.Find("Resolution").GetComponent<Dropdown>();
+            textureQualityDropdown = GameObject.Find("Texture Quality").GetComponent<Dropdown>();
+            antialiasingDropdwon = GameObject.Find("Antialiasing").GetComponent<Dropdown>();
+            gammaSlider = GameObject.Find("Gamma Slider").GetComponent<Slider>();
+            masterVolumeSlider = GameObject.Find("Master Volume Slider").GetComponent<Slider>();
+            bgmMusicVolumeSlider = GameObject.Find("Music Volume Slider").GetComponent<Slider>();
+            sfxVolumeSlider = GameObject.Find("SFX Volume Slider").GetComponent<Slider>();
+            masterVolumeMuteToggle = GameObject.Find("Master Volume Mute").GetComponent<Toggle>();
+            bgmMusicMuteToggle = GameObject.Find("SFX Volume Mute").GetComponent<Toggle>();
+            sfxMuteToggle = GameObject.Find("Music Volume Mute").GetComponent<Toggle>();
+            applyButton = GameObject.Find("Apply").GetComponent<Button>();
 
-        fullscreenToggle = GameObject.Find("Fullscreen").GetComponent<Toggle>();
-        resolutionDropdown = GameObject.Find("Resolution").GetComponent<Dropdown>();
-        textureQualityDropdown = GameObject.Find("Texture Quality").GetComponent<Dropdown>();
-        antialiasingDropdwon = GameObject.Find("Antialiasing").GetComponent<Dropdown>();
-        masterVolumeSlider = GameObject.Find("Master Volume Slider").GetComponent<Slider>();
-        bgmMusicVolumeSlider = GameObject.Find("Music Volume Slider").GetComponent<Slider>();
-        sfxVolumeSlider = GameObject.Find("SFX Volume Slider").GetComponent<Slider>();
-        masterVolumeMuteToggle = GameObject.Find("Master Volume Mute").GetComponent<Toggle>();
-        bgmMusicMuteToggle = GameObject.Find("SFX Volume Mute").GetComponent<Toggle>();
-        sfxMuteToggle = GameObject.Find("Music Volume Mute").GetComponent<Toggle>();
-        applyButton = GameObject.Find("Apply").GetComponent<Button>();
+            fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        vSyncToggle.onValueChanged.AddListener(delegate { OnVSyncChange(); });
+        motionBlueToggle.onValueChanged.AddListener(delegate { OnMotionBlurChange(); });
+        postProcessingToggle.onValueChanged.AddListener(delegate { OnPostProcessingChange(); });
+            resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+            textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureChange(); });
+            antialiasingDropdwon.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
+        gammaSlider.onValueChanged.AddListener(delegate { OnGammaChange(); });
+            masterVolumeSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChange(); });
+            bgmMusicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+            sfxVolumeSlider.onValueChanged.AddListener(delegate { OnSFXVolumeChange(); });
+            masterVolumeMuteToggle.onValueChanged.AddListener(delegate { OnMasterVolumeMute(); });
+            bgmMusicMuteToggle.onValueChanged.AddListener(delegate { OnMusicMute(); });
+            sfxMuteToggle.onValueChanged.AddListener(delegate { OnSFXMute(); });
+            applyButton.onClick.AddListener(delegate { OnApplyButtonClick(); });
 
-        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
-        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
-        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureChange(); });
-        antialiasingDropdwon.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
-        masterVolumeSlider.onValueChanged.AddListener(delegate { OnMasterVolumeChange(); });
-        bgmMusicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
-        sfxVolumeSlider.onValueChanged.AddListener(delegate { OnSFXVolumeChange(); });
-        masterVolumeMuteToggle.onValueChanged.AddListener(delegate { OnMasterVolumeMute(); });
-        bgmMusicMuteToggle.onValueChanged.AddListener(delegate { OnMusicMute(); });
-        sfxMuteToggle.onValueChanged.AddListener(delegate { OnSFXMute(); });
-        applyButton.onClick.AddListener(delegate { OnApplyButtonClick(); });
-
-        resolutions = Screen.resolutions;
-        foreach(Resolution resolution in resolutions)
-        {
-            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
-        }
-
+            resolutions = Screen.resolutions;
+            foreach (Resolution resolution in resolutions)
+            {
+                resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+            }
+        
         soundMan = SoundManager.GetInstance();
 
         LoadSettings();
 
-        soundsMenu.SetActive(false);
-
     }
+
+
 
     public void OnFullscreenToggle()
     {
         gameSettings.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void OnVSyncChange()
+    {
+        if (vSyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = gameSettings.vSync = 1;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = gameSettings.vSync = 0;
+        }
+    }
+
+    public void OnMotionBlurChange()
+    {
+        if (motionBlueToggle.isOn)
+        {
+            gameSettings.motionBlur = postProcessing.motionBlur.enabled = true;
+        }
+        else
+        {
+            gameSettings.motionBlur = postProcessing.motionBlur.enabled = false;
+        }
+    }
+
+    public void OnPostProcessingChange()
+    {
+        if (postProcessingToggle.isOn)
+        {
+            gameSettings.postProcessingBool = postProcessing.bloom.enabled = true;
+            postProcessing.vignette.enabled = true;
+        }
+        else
+        {
+            gameSettings.postProcessingBool = postProcessing.bloom.enabled = false;
+            postProcessing.vignette.enabled = false;
+        }
     }
 
     public void OnResolutionChange()
@@ -94,6 +152,11 @@ public class SettingsManager : MonoBehaviour
     {
         QualitySettings.antiAliasing = (int)Mathf.Pow(2.0f, antialiasingDropdwon.value);
         gameSettings.antialiasing = antialiasingDropdwon.value;
+    }
+
+    public void OnGammaChange()
+    {
+
     }
 
     public void OnMasterVolumeChange()
@@ -164,11 +227,11 @@ public class SettingsManager : MonoBehaviour
 
     public void SaveSettings()
     {
-        graphicsMenu.SetActive(true);
-        soundsMenu.SetActive(true);
+        //graphicsMenu.SetActive(true);
+        //soundsMenu.SetActive(true);
         string jsonData = JsonUtility.ToJson(gameSettings, true);
         File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
-        soundsMenu.SetActive(false);
+        //soundsMenu.SetActive(false);
     }
 
     public void LoadSettings()
@@ -177,9 +240,15 @@ public class SettingsManager : MonoBehaviour
         gameSettings = JsonUtility.FromJson<GameSettings>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
         fullscreenToggle.isOn = gameSettings.fullscreen;
         Screen.fullScreen = gameSettings.fullscreen;
+        //******************** Magic **************************
+        vSyncToggle.isOn = gameSettings.vSync != 0;
+        //******************** Magic **************************
+        motionBlueToggle.isOn = gameSettings.motionBlur;
+        postProcessingToggle.isOn = gameSettings.postProcessingBool;
         resolutionDropdown.value = gameSettings.resolutionIndex;
         textureQualityDropdown.value = gameSettings.textureQuality;
         antialiasingDropdwon.value = gameSettings.antialiasing;
+        gammaSlider.value = gameSettings.gamma;
         masterVolumeSlider.value = gameSettings.masterVolume;
         bgmMusicVolumeSlider.value = gameSettings.bgmMusicVolume;
         sfxVolumeSlider.value = gameSettings.sfxVolume;
