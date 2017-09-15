5e26fe1c6546796cc056131eb5cfd59f76376aba
Addison Stewart <addison.stewart@hotmail.com>
2017-09-15 15:23:28 +1000

    In the process of fixing options menu

---
+++ Bullet Ballet Prototype/Assets/Scripts/WeaponSelectMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelectMenu : MonoBehaviour {
+
+    WeaponTypes weapon1;
+    WeaponTypes weapon2;
+
+
+	void Start ()
+    {
+        weapon1 = WeaponTypes.AI_BerettaM9;
+        weapon2 = WeaponTypes.AI_ColtPhthon357;
+	}
+	
+    //void Weapon1()
+    //{
+    //    WeaponShooter.m_WeaponType = weapon1;
+    //}
+
+    //void Weapon2()
+    //{
+    //    WeaponShooter.m_WeaponType = weapon2;
+    //}
+}
