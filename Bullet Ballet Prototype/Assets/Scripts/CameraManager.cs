fd2cd5fbb408f5002ddb2e50e9f9f51e29d92503
Addison Stewart <addison.stewart@hotmail.com>
2017-10-12 16:28:54 +1100

    fixed small bug with null reference on camera manager script

--- Bullet Ballet Prototype/Assets/Scripts/CameraManager.cs
+++ Bullet Ballet Prototype/Assets/Scripts/CameraManager.cs
@@ -145,7 +145,7 @@ public class CameraManager : MonoBehaviour {
         m_EventSystem = GameObject.Find("EventSystem");
 
         m_PlayerTransform = GameObject.Find("Player (Rigged)").transform;
-        m_WaypointMarkerTransform = GameObject.Find("Waypoint System").transform;
+        m_WaypointMarkerTransform = GameObject.Find("Waypoint System").transform.GetChild(1);
         m_PlayerArmLeft = GameObject.Find("Player (Rigged)").transform.GetChild(0).GetChild(1);
         m_PlayerArmRight = GameObject.Find("Player (Rigged)").transform.GetChild(0).GetChild(0);
 
