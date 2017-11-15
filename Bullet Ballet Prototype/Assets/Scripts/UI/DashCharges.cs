23e6c2ca06b0e5d2aa01243a95691827c8c1989c
James Owen <jamesowen012@gmail.com>
2017-11-15 16:24:32 +1100

    Changed how the Dash UI works
    Now has a slider moving up while still using the same image swap to show how many are left
    added the mask controls to UIManager
    Updated the canvas prefab to have the slider mask and made it swap between the new images

--- Bullet Ballet Prototype/Assets/Scripts/UI/DashCharges.cs
+++ Bullet Ballet Prototype/Assets/Scripts/UI/DashCharges.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class DashCharges : MonoBehaviour
 {
 
-    public int currentCharges;
+    private int currentCharges;
     int previousCharges;
 
     GameObject chargesUI;
@@ -14,38 +14,52 @@ public class DashCharges : MonoBehaviour
     public Texture chargeUsed;
     public Texture chargeReady;
 
-	void Start ()
-    {
+    private PlayerDive m_PlayerDive;
+    private Transform m_DashTransform;
 
-        currentCharges = GameObject.Find("Player (Rigged)").GetComponent<PlayerDive>().m_DashChargesCurrent;
+    public Slider m_DashSlider;
+
+    void Start ()
+    {
+        m_PlayerDive = GameObject.Find("Player (Rigged)").GetComponent<PlayerDive>();
+        m_DashTransform = transform;
+
+        currentCharges = m_PlayerDive.m_DashChargesCurrent;
         previousCharges = currentCharges;
 	}
 	
 	void Update ()
     {
-        currentCharges = GameObject.Find("Player (Rigged)").GetComponent<PlayerDive>().m_DashChargesCurrent;
+        float percentage = 1 - (m_PlayerDive.m_DashChargeTimerCurrent / m_PlayerDive.m_DashChargeTimerMax);
+        float value = 1- ((percentage / m_PlayerDive.m_DashChargesMax) + m_PlayerDive.m_DashChargesCurrent/(float)m_PlayerDive.m_DashChargesMax); 
+        //print(value);
+        m_DashSlider.value = value;
 
-        chargesUI = GameObject.Find("Canvas").transform.GetChild(0).GetChild(3).GetChild(currentCharges).transform.gameObject;
 
+        currentCharges = m_PlayerDive.m_DashChargesCurrent;
+
+        chargesUI = m_DashTransform.GetChild(currentCharges+1).transform.gameObject;
         
         //Make charge show as used on UI
         if (currentCharges < previousCharges)
         {
-            chargesUI.GetComponent<RawImage>().texture = chargeUsed;
+            if(previousCharges == m_PlayerDive.m_DashChargesMax) {
+                chargesUI.SetActive(false);
+            } else { 
+                chargesUI.GetComponent<RawImage>().texture = chargeUsed;
+            }
         }
         else if (currentCharges > previousCharges)
         {
-            chargesUI = GameObject.Find("Canvas").transform.GetChild(0).GetChild(3).GetChild(previousCharges).transform.gameObject;
-            chargesUI.GetComponent<RawImage>().texture = chargeReady;
+            chargesUI = m_DashTransform.GetChild(previousCharges + 1).gameObject;
+            if (currentCharges == m_PlayerDive.m_DashChargesMax) {
+                chargesUI.SetActive(true);
+            } else {
+                chargesUI.GetComponent<RawImage>().texture = chargeReady;
+            }
         }
 
         previousCharges = currentCharges;
     }
 
-    void test()
-    {
-
-        
-        
-    }
 }
